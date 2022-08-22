a450c5799f71afca0acc366338c7619d207bdb86
shaqayq <shaqayq.darwazi@gmail.com>
2022-08-22 20:03:34 +0430

    Add navbar style

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,25 +1,29 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import logo from '../assests/logo.png';
+import '../style/navbar.css';
 
 const Navbar = () => (
   <div className="navbar">
     <div className="navlinks">
-
-      <h1>Space Travelers&apos; Hub</h1>
       <img className="logo" src={logo} alt="logo" />
+      <h1 className="title">Space Travelers&apos; Hub</h1>
       <ul className="holder">
         <li>
-          <Link to="/" className="link">Rockets</Link>
+          <Link to="/" className="link active">Rockets</Link>
         </li>
         <li>
           <Link to="/missions" className="link">Missions</Link>
+        </li>
+        <li>
+          |
         </li>
         <li>
           <Link to="/my-profile" className="link">My Profile</Link>
         </li>
       </ul>
     </div>
+    <hr/>
   </div>
 );
 
