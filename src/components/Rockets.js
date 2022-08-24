93b8c74ec5546248dea1351ed0cd97062869f7a7
Berkay Akbas <berkayakbas@gmail.com>
2022-08-24 12:35:56 +0300

    Fix merge errors

--- src/components/Rockets.js
+++ src/components/Rockets.js
@@ -1,15 +1,15 @@
 import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { fetchData } from '../redux/rockets/rocket';
-import RocketItem from './RocketItem';
 import '../style/rocket.css';
+import RocketItem from './RocketItem';
 
 const Rockets = () => {
   const rockets = useSelector((state) => state.rockets);
   const dispatch = useDispatch();
 
   useEffect(() => {
-    if(rockets.length === 0){
+    if (rockets.length === 0) {
       dispatch(fetchData());
     }
   }, []);
@@ -18,7 +18,7 @@ const Rockets = () => {
     <>
       <div className="rocketContiner">
         {
-          rockets.map((item) => (<RocketTable rockets={item} key={item.id} />))
+          rockets.map((item) => (<RocketItem rockets={item} key={item.id} />))
         }
       </div>
     </>
